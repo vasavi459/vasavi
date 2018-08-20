56950a9337675139f69ba36007c7d1798859dbb3
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-20 16:53:36 +0530

    Create three.py

---
+++ three.py
@@ -0,0 +1,13 @@
+m=int(input('enter a num'))
+n=int(input('enter a num'))
+o=int(input('enter a num'))
+if(m>n):
+        if(m>0):
+           print('m is greater')
+        else:
+           print('o is greater')
+else:
+     if(n>0):
+       print('n is greater')
+     else:
+       print('o is greater')
