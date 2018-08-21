cb3bc474a4f9cf86625cc8152e2652e4366ed6a8
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-21 16:00:44 +0530

    Update and rename sum.py to sum1.py

---
+++ sum1.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the value of n: "))
+v=num1
+sum = 0
+if num1 <= 0: 
+   print("Enter a whole positive number!") 
+else: 
+   while num1 > 0:
+        sum = sum + num1
+        num1 = num1 - 1
+print("Sum of first", v, "natural numbers is: ", sum)        
