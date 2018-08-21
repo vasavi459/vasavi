48edd663a7dc485b376951177f4f27302e0c58da
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-21 16:09:07 +0530

    Update and rename coun.py to coun1.py

--- coun1.py
+++ coun1.py
@@ -1,6 +1,6 @@
-Number = int(input("Please Enter any Number: "))
-h = 0
+Number = int(input(" "))
+q = 0
 while(Number > 0):
     Number = Number // 10
-    h = h + 1
+    q = q + 1
 print("\n Number of Digits in a Given Number = %d" %Count)    
