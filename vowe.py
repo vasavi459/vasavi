6d71fb3ab8e4062597ba88400a8b3921894a7646
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-21 14:35:44 +0530

    Update vowe.py

--- vowe.py
+++ vowe.py
@@ -2,8 +2,8 @@ p=raw_input('')
 o=['a','e','i','o','u','A','E','I','O','U']
 if(p>='a' and p<='z' or p>='A' and p<='Z'):
     if(p in o):
-          print('vowel')
+          print('Vowel')
     else:
-          print('consonant')
+          print('Consonant')
 else:
     print('invalid')
