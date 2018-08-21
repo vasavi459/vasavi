781077c9b6714f41cde5d17724bf87bda21b45b5
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-21 14:31:17 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,5 +1,5 @@
 p=raw_input('')
-n=['a','e','i','o','u',A','E','I','O','U']
+n=['a','e','i','o','u','A','E','I','O','U']
 if(p>='a' and p<='z' or p>='A' and p<='Z'):
     if(p in n):
           print('vowel')
