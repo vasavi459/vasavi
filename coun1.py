ac9c0fecc8d1646ceb96075ccdd3a22a78ea3aed
vasavi459 <41769990+vasavi459@users.noreply.github.com>
2018-08-21 16:13:11 +0530

    Update coun1.py

--- coun1.py
+++ coun1.py
@@ -3,4 +3,4 @@ q = 0
 while(Number > 0):
     Number = Number // 10
     q = q + 1
-print("\n Number of Digits in a Given Number = %d" %Count)    
+print("\n Number of Digits in a Given Number = %d" %q)    
